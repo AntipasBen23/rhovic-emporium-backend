24837ffb5dd728fd35923518be96072e33bf6fde
AntipasBen23 <benantipas@gmail.com>
2026-03-02 15:01:02 +0100

    added recover middleware

---
+++ internal/middleware/recover.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func Recoverer(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if v := recover(); v != nil {
+				log.Printf("panic: %v", v)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
